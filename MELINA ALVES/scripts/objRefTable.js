ae41d8b20d87edeab98bf8744748dc970a0894f2
abauna <48293205+abauna@users.noreply.github.com>
2025-02-19 13:38:43 -0300

    add codes

---
+++ MELINA ALVES/scripts/objRefTable.js
@@ -0,0 +1,70 @@
+const C3 = self.C3;
+self.C3_GetObjectRefTable = function () {
+	return [
+		C3.Plugins.Sprite,
+		C3.Plugins.Text,
+		C3.Plugins.Mouse,
+		C3.Behaviors.solid,
+		C3.Behaviors.scrollto,
+		C3.Behaviors.EightDir,
+		C3.Plugins.Mouse.Cnds.OnObjectClicked,
+		C3.Plugins.System.Acts.GoToLayout,
+		C3.Plugins.Sprite.Cnds.OnCollision
+	];
+};
+self.C3_JsPropNameTable = [
+	{Sprite: 0},
+	{Texto: 0},
+	{Sprite2: 0},
+	{Mouse: 0},
+	{Sprite3: 0},
+	{Texto2: 0},
+	{PLAY: 0},
+	{Sprite4: 0},
+	{Sprite5: 0},
+	{Sprite6: 0},
+	{Texto3: 0},
+	{Texto4: 0},
+	{Sprite7: 0},
+	{Sólido: 0},
+	{Sprite8: 0},
+	{Sprite9: 0},
+	{CentrarEm: 0},
+	{"8Direções": 0},
+	{Sprite10: 0},
+	{Sprite11: 0},
+	{Sprite12: 0},
+	{Texto5: 0},
+	{Sprite13: 0},
+	{Sprite14: 0},
+	{Sprite15: 0},
+	{Texto6: 0},
+	{Sprite16: 0}
+];
+
+self.InstanceType = {
+	Sprite: class extends self.ISpriteInstance {},
+	Texto: class extends self.ITextInstance {},
+	Sprite2: class extends self.ISpriteInstance {},
+	Mouse: class extends self.IInstance {},
+	Sprite3: class extends self.ISpriteInstance {},
+	Texto2: class extends self.ITextInstance {},
+	PLAY: class extends self.ISpriteInstance {},
+	Sprite4: class extends self.ISpriteInstance {},
+	Sprite5: class extends self.ISpriteInstance {},
+	Sprite6: class extends self.ISpriteInstance {},
+	Texto3: class extends self.ITextInstance {},
+	Texto4: class extends self.ITextInstance {},
+	Sprite7: class extends self.ISpriteInstance {},
+	Sprite8: class extends self.ISpriteInstance {},
+	Sprite9: class extends self.ISpriteInstance {},
+	Sprite10: class extends self.ISpriteInstance {},
+	Sprite11: class extends self.ISpriteInstance {},
+	Sprite12: class extends self.ISpriteInstance {},
+	Texto5: class extends self.ITextInstance {},
+	Sprite13: class extends self.ISpriteInstance {},
+	Sprite14: class extends self.ISpriteInstance {},
+	Sprite15: class extends self.ISpriteInstance {},
+	Texto6: class extends self.ITextInstance {},
+	Sprite16: class extends self.ISpriteInstance {}
+}
